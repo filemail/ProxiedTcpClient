3f337341c5c6aa59758786840a7f789626664ff3
Pawel Rychlicki <prychlicki@degree.no>
2020-05-21 16:17:58 +0200

    Initial implementation

---
+++ ProxiedTcpClient/Models/ProxyStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxiedTcpClient.Models
+{
+    public class ProxyStatus
+    {
+        public bool IsEnabled { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
